a741d916aef8207842169834a1bc76bca9c14eff
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-13 16:26:49 +0300

    Add project files.

---
+++ HomeworkClassLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace HomeWork2
+{
+    public static class Concatenationlib
+    {
+        public static string Concatenate(string name)
+        {
+            var currentTime = DateTime.Now.ToString("hh:mm:ss tt");
+            return $"{currentTime} Welcome {name} to dotnet world";
+        }
+    }
+}
